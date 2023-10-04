d3f125f76c706da5671a21a29be8bdebef66187c
AbdullaSafwan <abdullasafuwan@gmail.com>
2023-10-04 15:48:58 +0530

    hi

--- bin/Programs/sum of odd and even numbers.dart
+++ bin/Programs/sum of odd and even numbers.dart
@@ -5,5 +5,15 @@ void main(){
       sumofeven=sumofeven+i;
     }
   }
+  print('Sum of even numbers');
   print(sumofeven);
+
+  int sumofodd =0;
+  for(int i=1;i<=10;i++){
+    if(i%2!=0){
+      sumofodd=sumofodd+i;
+    }
+  }
+  print('sum of odd numbers');
+  print(sumofodd);
 }
