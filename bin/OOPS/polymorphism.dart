8e44bd606419f33890264954983a21b89977470f
AbdullaSafwan <abdullasafuwan@gmail.com>
2023-10-11 15:39:46 +0530

    0

--- bin/OOPS/polymorphism.dart
+++ bin/OOPS/polymorphism.dart
@@ -1,13 +1,21 @@
 class bank{
-  void details(String name, String location){
+  void details(String name, String ifsc){
     print('Name     : $name');
-    print('Location : $location');
+    print('Location : $ifsc');
   }
 }
 
 class Childbank extends bank{
   @override
-  void details(String name, String location) {
+  void details(String branch, String accounttype) {
+    super.details('HDFC', "HDFC0002158");
+    print('Branch   : $branch');
+    print('Acc.Type : $accounttype');
+
 
   }
+}
+void main(){
+  Childbank obj = Childbank();
+  obj.details('Kakkanad', 'Savings');
 }
